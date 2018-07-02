ddc95dd98b44c4c89f6042cc2b75808cb3f18498
Drew <drewfisher1234@gmail.com>
2018-07-02 14:36:01 -0400

    Completed Assignment

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var oScore = 0;
+var xScore = 0;
 var currentPlayer = "X";
 var nextPlayer = "O";
 
@@ -16,12 +18,74 @@ const winningCombinations = [
 ]
 
 handleClick = function (event) {
-    var cell = event.target
-    console.log(cell.id);
+    var cell = event.target;
+    if(cell.innerHTML != ""){
+        alert("Click on an empty box!!!!!");
+        return;
+    }
+    cell.innerHTML = currentPlayer;
+
+    if (currentPlayer === "X") {
+        playerSelections = playerXSelections;
+        nextPlayer = "O";
+    } else {
+        playerSelections = playerOSelections;
+        nextPlayer = "X";
+    }
+    playerSelections.push(parseInt(cell.id));
+    console.log(cell.innerHTML);
+
+    let check = checkWinner();
+    if (check === true) {
+        alert("Player "+currentPlayer+" has won!");
+        if(currentPlayer=="X"){
+            xScore++;
+            xScoreLabel.textContent = xScore;
+        }else if(currentPlayer == "O"){
+            oScore++;
+            oScoreLabel.textContent = oScore;
+        }
+        resetGame();
+
+    }else if(check === false){
+        if(playerOSelections.length>=5 || playerXSelections.length>=5){
+            alert("Draw");
+            resetGame();
+        }
+    }
+
+    // Swap players
+    currentPlayer = nextPlayer;
+
 }
 
 var cells = document.querySelectorAll("td");
 
 for (var i = 0; i < cells.length; i++) {
-    cells[i].addEventListener('click', handleClick)
+    cells[i].addEventListener('click', handleClick);
+}
+
+function checkWinner() {
+    for (let combo of winningCombinations) {
+        var matches = 0;
+        for (let item of playerSelections) {
+            if (combo.includes(item)) {
+                matches++;
+            }
+        }
+        if (matches == 3) {
+            return (true);
+        }
+    }
+    return (false);
+}
+
+function resetGame() {
+    playerXSelections = new Array();
+    playerOSelections = new Array();
+    currentPlayer = "O";
+    nextPlayer = "X"
+    for(var i = 0; i < cells.length; i++) {
+      cells[i].innerHTML = ""
+    }
 }
